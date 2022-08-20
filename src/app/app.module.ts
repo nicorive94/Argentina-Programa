4f8223c5768e350b77b129d5683c7a0a2acf20f4
nicorive94 <nicodanielrivero94@gmail.com>
2022-08-20 18:23:48 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { BannerComponent } from './banner/banner.component';
+import { PerfilComponent } from './perfil/perfil.component';
+import { EstudiosComponent } from './estudios/estudios.component';
+import { ExperienciasComponent } from './experiencias/experiencias.component';
+import { SkilsComponent } from './skils/skils.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    BannerComponent,
+    PerfilComponent,
+    EstudiosComponent,
+    ExperienciasComponent,
+    SkilsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
